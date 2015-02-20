522336f8c6cf6aa3c9798c3469d6e0d338e26061
boonchu <bigchoo@gmail.com>
2015-02-20 13:20:12 -0800

    Updated Person Class

--- Moose/Person.pl
+++ Moose/Person.pl
@@ -2,12 +2,23 @@
 
 package Person;
 use Moose;
+use Carp qw( confess );
 
 has name => ( is => 'rw', isa => 'Str' );
+has age  => ( is => 'rw', isa => 'Num' );
+
+sub BUILD {
+	my ($self) = @_;
+
+	if ($self->name eq 'boonchu') {
+		confess 'Boonchu is not allowed for using Person class';
+	}
+	return;
+}
 
 sub hello {
         my $self = shift;
-        print "Hello. My name is ", $self->name, "\n";
+        print "Hello. My name is ", $self->name, ". I am about ", $self->age, " year old\n";
 }
 
 no Moose;
@@ -18,6 +29,9 @@ __PACKAGE__->meta->make_immutable;
 package main;
 use strict;
 use warnings;
+use v5.16;
 
-my $b = new Person(name => "boonchu");
+my $a = new Person(name => "alex", age => 25);
+my $b = new Person(name => "boonchu", age => 40);
+$a->hello;
 $b->hello;
